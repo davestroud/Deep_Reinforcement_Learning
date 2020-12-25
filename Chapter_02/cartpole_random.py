4cf273f548891cb9176159fb4e6e8a2fae64c5b6
dstroud <david@davidstroud.me>
2020-12-25 11:44:01 -0600

    finish cartpole random

--- Chapter_02/cartpole_random.py
+++ Chapter_02/cartpole_random.py
@@ -6,3 +6,14 @@ if __name__ == "__main__":
     total_reward = 0.0
     total_steps = 0
     obs = env.reset()
+
+    while True:
+        action = env.action_space.sample()
+        obs, reward, done, _ = env.step(action)
+        total_reward += reward
+        total_steps += 1
+        if done:
+            break
+
+    print("Episode done in %d steps, total reward %.2f" % (
+        total_steps, total_reward))
