7e8717e6e182d914b632597d41c6f8a458ac3e04
dstroud <david@davidstroud.me>
2020-12-27 11:47:04 -0600

    add action function

--- Chapter_02/random_action_wrapper.py
+++ Chapter_02/random_action_wrapper.py
@@ -7,3 +7,9 @@ class RandomActionWrapper(gym.ActionWrapper):
     def __init__(self, env, epsilon=0.1):
         super(RandomActionWrapper, self).__init__(env)
         self.epsilon = epsilon
+
+    def action(self, action:Action) -> Action:
+        if random.random() < self.epsilon:
+            print("Random!")
+            return self.env.action_space.sample()
+        return action
