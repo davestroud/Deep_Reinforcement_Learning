b7f4af7b53af5d538476e07344f87db93e85a632
dstroud <david@davidstroud.me>
2020-12-29 11:13:09 -0600

    add cuda code

--- PyTorch/Modules.py
+++ PyTorch/Modules.py
@@ -21,7 +21,10 @@ class OurModule(nn.Module):
 
 if __name__ == "__main__":
     net = OurModule(num_inputs=2, num_classes=3)
+    print(net)
     v = torch.FloatTensor([[2, 3]])
     out = net(v)
-    print(net)
     print(out)
+    print("Cuda's availability is %s" % torch.cuda.is_available())
+    if torch.cuda.is_available():
+        print("Data from cuda: %s" % out.to('cuda'))
