97f8b36f912075414166173a7561a2e9967467d4
dstroud <david@davidstroud.me>
2021-01-02 14:06:52 -0600

    add cartpole code for cross entropy portion

---
+++ Cross_Entropy/cartpole.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import gym
+from collections import namedtuple
+import numpy as np
+from tensorboardX import SummaryWriter
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
