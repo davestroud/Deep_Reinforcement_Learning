a8479051c07690648443f27d5e587f4bc4a7d2c9
dstroud <david@davidstroud.me>
2021-01-03 17:24:13 -0600

    add net class

--- Cross_Entropy/cartpole.py
+++ Cross_Entropy/cartpole.py
@@ -14,3 +14,15 @@ HIDDEN_SIZE = 128
 BATCH_SIZE = 16
 PERCENTILE = 70
 
+
+class Net(nn.Module):
+    def __init__(self, obs_size, hidden_size, n_actions):
+        super(Net, self).__init__()
+        self.net = nn.Sequential(
+            nn.Linear(obs_size, hidden_size),
+            nn.ReLU(), 
+            nn.Linear(hidden_size, n_actions)
+        )
+
+    def forward(self, x):
+        return self.net(x)
