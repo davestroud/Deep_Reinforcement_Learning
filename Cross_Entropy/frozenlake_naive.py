ace4da2dc7c26f9b5cc41da39c9ef8b65af902e0
dstroud <david@davidstroud.me>
2021-01-05 16:36:45 -0600

    add discrete one hot wrapper class

--- Cross_Entropy/frozenlake_naive.py
+++ Cross_Entropy/frozenlake_naive.py
@@ -13,3 +13,19 @@ BATCH_SIZE = 16
 PERCENTILE = 70
 
 
+e = gym.make('FrozenLake-v0')
+e.observation_space
+e.action_space
+e.reset
+
+e.render()
+
+
+class DiscreteOneHotWrapper(gym.ObservationWrapper):
+    def __init__(self, env):
+        super(DiscreteOneHotWrapper, self).__init__(env)
+        assert isinstance(env.observation_space, gym.spaces.Discrete)
+        shape = (env.observation_space.n, )
+        self.observation_space = gym.spaces.Box(
+            0.0, 1.0, shape, dtype =np.float32
+        )
