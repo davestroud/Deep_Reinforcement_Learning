ec037d3d0499bf20ca79dd64f63c89f676b93a27
dstroud <david@davidstroud.me>
2021-01-05 15:18:41 -0600

    add libraries

---
+++ Cross_Entropy/frozenlake_naive.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import gym, gym.spaces
+from collections import namedtuple
+import numpy as np
+from tensorboardX import SummaryWriter
+
+import torch
+import torch.nn as nn
+
